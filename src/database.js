ca0c81802ffc3e9244aaa08759a914dd4b3f9515
manueljesus00 <mjfloresmontano@gmail.com>
2020-07-31 19:31:30 +0200

    Upload

--- src/database.js
+++ src/database.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/notes-db-app', {
     useCreateIndex: true,
     useNewUrlParser: true,
+    useUnifiedTopology: true,
     useFindAndModify: false
 })
     .then(db => console.log('Mongo DB is connect'))
--- src/routes/notes.js
+++ src/routes/notes.js
@@ -1,8 +1,32 @@
 const express = require('express');
 const router = express.Router();
 
+router.get('/notes/add', (req, res) => {
+    res.render('notes/new-note');
+});
+
 router.get('/notes', (req, res) => {
     res.send('Notes from database');
+});
+
+router.post('/notes/new-note', (req, res) => {
+    const { title, description } = req.body;
+    const errors = [];
+    if(!title){
+        errors.push({text: 'Escribe el título'});
+    }
+    if (!description){
+        errors.push({text: 'Escribe una descripcion'});
+    }
+    if(errors.length > 0){
+        res.render('notes/new-note', {
+            errors,
+            title,
+            description
+        });
+    } else {
+        res.send('ok');
+    }
 });
 
 module.exports = router;
